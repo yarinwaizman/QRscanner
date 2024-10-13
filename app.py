132fafbc0876e6fadccca9ee14b12d2231ceecfd
Yarin waizman <yarinwaizman1996@gmail.com>
2024-10-13 13:11:35 +0300

    Your commit message

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def submit_data():
     return jsonify({'message': 'Data saved successfully!'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+

a94abff8cea6425c0161affbb77c37ef671eb8c2
Yarin waizman <yarinwaizman1996@gmail.com>
2024-10-16 15:19:22 +0300

    db_update

---
+++ testphp.php
@@ -0,0 +1,12 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
+echo json_encode(['status' => 'success', 'message' => 'Test endpoint works!']);
+?>
